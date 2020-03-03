277eeb25d95800fca58136bda02196f69e6332ff
TempestMK7 <david.greer@hcss.com>
2020-03-02 18:30:32 -0600

    Added shell of portal scene.

---
+++ Assets/Scripts/PortalSceneManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PortalSceneManager : MonoBehaviour {
+
+    public Text nameText;
+    public Text levelText;
+    public Text summonText;
+
+    private AccountStateContainer state;
+
+    public void Awake() {
+        state = StateManager.GetCurrentState();
+        nameText.text = state.playerName;
+        levelText.text = string.Format("Level {0}", state.currentLevel);
+        summonText.text = state.currentScrolls.ToString("0");
+    }
+
+    public void OnBackPressed() {
+        SceneManager.LoadScene("HubScene");
+    }
+
+    public void OnSummon() {
+
+    }
+
+    public void OnSummonTen() {
+
+    }
+}
