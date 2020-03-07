f0e5d0f392e82c84ea2e4730749cb83359087140
TempestMK7 <david.greer@hcss.com>
2020-03-07 00:25:19 -0600

    Changed some icons.

--- Assets/Scripts/StateComponents/HeroEnum.cs
+++ Assets/Scripts/StateComponents/HeroEnum.cs
@@ -59,123 +59,123 @@ public class BaseHero {
     public static BaseHero GetHero(HeroEnum hero) {
         switch (hero) {
             case HeroEnum.VAPOR_CLOUD:
-                return new BaseHero(hero, "Vapor Cloud", "Icons/icon_gift",
+                return new BaseHero(hero, "Vapor Cloud", "Icons/Element02_256_04",
                     RoleEnum.DAMAGE, FactionEnum.WATER, 1,
                     60, 60, 80, 32, 40, 65, 0.1);
             case HeroEnum.RAIN_MAN:
-                return new BaseHero(hero, "Rain Man", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Rain Man", "Icons/Element02_256_04",
                     RoleEnum.SUPPORT, FactionEnum.WATER, 2,
                     75, 60, 70, 38, 42, 60, 0.1);
             case HeroEnum.FEESH:
-                return new BaseHero(hero, "Feesh", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Feesh", "Icons/Element02_256_04",
                     RoleEnum.PROTECTION, FactionEnum.WATER, 3,
                     85, 70, 50, 48, 42, 50, 0.1);
             case HeroEnum.MIST_CALLER:
-                return new BaseHero(hero, "Mist Caller", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Mist Caller", "Icons/Element02_256_04",
                     RoleEnum.DAMAGE, FactionEnum.WATER, 5,
                     60, 55, 95, 35, 40, 75, 0.15);
             case HeroEnum.ORACLE:
-                return new BaseHero(hero, "Oracle", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Oracle", "Icons/Element02_256_04",
                     RoleEnum.SUPPORT, FactionEnum.WATER, 5,
                     75, 60, 90, 38, 42, 75, 0.15);
             case HeroEnum.CANDLE_MAN:
-                return new BaseHero(hero, "Candle Man", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Candle Man", "Icons/Element02_256_01",
                     RoleEnum.DAMAGE, FactionEnum.FIRE, 1,
                     55, 60, 85, 31, 40, 85, 0.15);
             case HeroEnum.EMBER:
-                return new BaseHero(hero, "Ember", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Ember", "Icons/Element02_256_01",
                     RoleEnum.SUPPORT, FactionEnum.FIRE, 2,
                     70, 55, 90, 34, 38, 80, 0.15);
             case HeroEnum.TORCH:
-                return new BaseHero(hero, "Torch", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Torch", "Icons/Element02_256_01",
                     RoleEnum.DAMAGE, FactionEnum.FIRE, 3,
                     65, 65, 95, 34, 40, 70, 0.2);
             case HeroEnum.LAVA_GOLEM:
-                return new BaseHero(hero, "Lava Golem", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Lava Golem", "Icons/Element02_256_01",
                     RoleEnum.PROTECTION, FactionEnum.FIRE, 4,
                     85, 80, 60, 48, 40, 60, 0.1);
             case HeroEnum.INFERNOMANCER:
-                return new BaseHero(hero, "Infernomancer", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Infernomancer", "Icons/Element02_256_01",
                     RoleEnum.DAMAGE, FactionEnum.FIRE, 5,
                     70, 60, 100, 32, 35, 95, 0.25);
             case HeroEnum.HAPPY_FLOWER:
-                return new BaseHero(hero, "Happy Flower", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Happy Flower", "Icons/Element02_256_10",
                     RoleEnum.SUPPORT, FactionEnum.GRASS, 1,
                     70, 70, 55, 38, 36, 55, 0.1);
             case HeroEnum.BUSH_WHACKER:
-                return new BaseHero(hero, "Bush Whacker", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Bush Whacker", "Icons/Element02_256_10",
                     RoleEnum.DAMAGE, FactionEnum.GRASS, 2,
                     75, 80, 60, 36, 34, 80, 0.2);
             case HeroEnum.BALL_OF_ROOTS:
-                return new BaseHero(hero, "Ball Of Roots", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Ball Of Roots", "Icons/Element02_256_10",
                     RoleEnum.PROTECTION, FactionEnum.GRASS, 3,
                     90, 65, 60, 36, 34, 80, 0.15);
             case HeroEnum.DRYAD:
-                return new BaseHero(hero, "Dryad", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Dryad", "Icons/Element02_256_10",
                     RoleEnum.SUPPORT, FactionEnum.GRASS, 4,
                     80, 85, 85, 38, 38, 80, 0.15);
             case HeroEnum.ANGERY_TREANT:
-                return new BaseHero(hero, "Angery Treant", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Angery Treant", "Icons/Element02_256_10",
                     RoleEnum.PROTECTION, FactionEnum.GRASS, 5,
                     100, 75, 60, 46, 42, 50, 0.1);
             case HeroEnum.SNOW_MAN:
-                return new BaseHero(hero, "Snow Man", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Snow Man", "Icons/Element02_256_19",
                     RoleEnum.SUPPORT, FactionEnum.ICE, 1,
                     70, 70, 65, 36, 40, 60, 0.1);
             case HeroEnum.ICE_CUBE:
-                return new BaseHero(hero, "Ice Cube", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Ice Cube", "Icons/Element02_256_19",
                     RoleEnum.PROTECTION, FactionEnum.ICE, 2,
                     85, 75, 55, 40, 45, 55, 0.1);
             case HeroEnum.ICICLE_FLINGER:
-                return new BaseHero(hero, "Icicle Flinger", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Icicle Flinger", "Icons/Element02_256_19",
                     RoleEnum.DAMAGE, FactionEnum.ICE, 3,
                     65, 90, 60, 35, 38, 85, 0.2);
             case HeroEnum.BLIZZARD_WIZZARD:
-                return new BaseHero(hero, "Blizzard Wizard", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Blizzard Wizard", "Icons/Element02_256_19",
                     RoleEnum.SUPPORT, FactionEnum.ICE, 4,
                     75, 60, 95, 32, 38, 90, 0.25);
             case HeroEnum.REFLECTOR:
-                return new BaseHero(hero, "Reflector", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Reflector", "Icons/Element02_256_19",
                     RoleEnum.PROTECTION, FactionEnum.ICE, 5,
                     95, 75, 80, 42, 50, 55, 0.15);
             case HeroEnum.STATIC_CLING:
-                return new BaseHero(hero, "Static Cling", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Static Cling", "Icons/Element02_256_16",
                     RoleEnum.DAMAGE, FactionEnum.ELECTRIC, 1,
                     55, 60, 80, 32, 38, 85, 0.15);
             case HeroEnum.BATTERY:
-                return new BaseHero(hero, "Battery", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Battery", "Icons/Element02_256_16",
                     RoleEnum.SUPPORT, FactionEnum.ELECTRIC, 2,
                     65, 55, 85, 40, 38, 80, 0.1);
             case HeroEnum.SPARK_ELEMENTAL:
-                return new BaseHero(hero, "Spark Elemental", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Spark Elemental", "Icons/Element02_256_16",
                     RoleEnum.DAMAGE, FactionEnum.ELECTRIC, 3,
                     60, 60, 85, 33, 39, 90, 0.2);
             case HeroEnum.LIGHTNING_WRAITH:
-                return new BaseHero(hero, "Lightning Wraith", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Lightning Wraith", "Icons/Element02_256_16",
                     RoleEnum.DAMAGE, FactionEnum.ELECTRIC, 4,
                     65, 65, 95, 32, 42, 85, 0.2);
             case HeroEnum.NEUROMANCER:
-                return new BaseHero(hero, "Neuromancer", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Neuromancer", "Icons/Element02_256_16",
                     RoleEnum.SUPPORT, FactionEnum.ELECTRIC, 5,
                     70, 60, 90, 34, 42, 90, 0.25);
             case HeroEnum.PEBBLE_ELEMENTAL:
-                return new BaseHero(hero, "Pebble Elemental", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Pebble Elemental", "Icons/Element02_256_22",
                     RoleEnum.PROTECTION, FactionEnum.EARTH, 1,
                     80, 75, 50, 45, 35, 55, 0.1);
             case HeroEnum.BOULDER:
-                return new BaseHero(hero, "Boulder", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Boulder", "Icons/Element02_256_22",
                     RoleEnum.PROTECTION, FactionEnum.EARTH, 2,
                     90, 70, 66, 46, 38, 60, 0.1);
             case HeroEnum.PEBBLE_FLINGER:
-                return new BaseHero(hero, "Pebble Flinger", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Pebble Flinger", "Icons/Element02_256_22",
                     RoleEnum.DAMAGE, FactionEnum.EARTH, 3,
                     70, 85, 60, 38, 32, 75, 0.1);
             case HeroEnum.LIVING_WALL:
-                return new BaseHero(hero, "Living Wall", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Living Wall", "Icons/Element02_256_22",
                     RoleEnum.PROTECTION, FactionEnum.EARTH, 4,
                     95, 75, 60, 48, 38, 50, 0.1);
             case HeroEnum.EARTHZERKER:
-                return new BaseHero(hero, "Earthzerker", "Icons/icon_gift_blue",
+                return new BaseHero(hero, "Earthzerker", "Icons/Element02_256_22",
                     RoleEnum.DAMAGE, FactionEnum.EARTH, 5,
                     75, 100, 50, 38, 31, 85, 0.2);
             default:
