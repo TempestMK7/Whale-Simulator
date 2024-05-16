f58e51e493696e8c52446c68653bb862be6fcea2
TempestMK7 <tempestmk7@gmail.com>
2024-05-16 13:09:00 -0500

    Final polish on Rezakaht 3.

--- Assets/Scripts/Views/HeroBehaviors/Rezakaht3Animation.cs
+++ Assets/Scripts/Views/HeroBehaviors/Rezakaht3Animation.cs
@@ -2,24 +2,26 @@ using UnityEngine;
 
 public class Rezakaht3Animation : HarshByteAnimation {
 
-    // public ParticleSystem specialParticle;
+    public Transform spriteTransform;
+    public ParticleSystem deathParticle;
 
     public override void OnCreate(Vector3 localScale) {
         base.OnCreate(localScale);
-/*        var particles = GetComponentsInChildren<ParticleSystem>();
-        Vector3 particleScale = localScale * 0.8f;
+        var particles = GetComponentsInChildren<ParticleSystem>();
+        Vector3 particleScale = localScale * 2f;
         foreach (ParticleSystem particle in particles) {
             particle.transform.localScale = particleScale;
-        }*/
-    }
-
-    public override void Special() {
-        base.Special();
-        // specialParticle.Play();
+        }
     }
 
     public override void Death() {
         base.Death();
-        // specialParticle.Play();
+        StartCoroutine(PlayDeathAnimation());
+    }
+
+    private System.Collections.IEnumerator PlayDeathAnimation() {
+        deathParticle.Play();
+        yield return new WaitForSeconds(0.3f);
+        spriteTransform.localScale = new Vector3(0f, 0f, 0f);
     }
 }
