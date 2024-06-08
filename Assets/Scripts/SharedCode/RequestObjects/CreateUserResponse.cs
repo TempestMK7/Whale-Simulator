d0daee88e8c2de0bc8630e8cb48f5ec5510b9339
TempestMK7 <tempestmk7@gmail.com>
2024-06-08 13:31:23 -0500

    Finished basic account creation and retrieval.

--- Assets/Scripts/SharedCode/RequestObjects/CreateUserResponse.cs
+++ Assets/Scripts/SharedCode/RequestObjects/CreateUserResponse.cs
@@ -5,8 +5,6 @@ namespace Com.Tempest.Whale.RequestObjects {
     [Serializable]
     class CreateUserResponse {
 
-        public string UserGuid { get; set; }
-        public string RefreshToken { get; set; }
         public string AccessToken { get; set; }
     }
 }
